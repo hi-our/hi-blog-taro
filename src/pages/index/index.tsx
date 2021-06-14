bbad21b5b52c66d442bafccfed8495f8353edc61
shenghanqin <shenghanqin@163.com>
2021-06-14 16:26:14 +0800

    模块搭建

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,25 +2,10 @@ import { Component } from 'react'
 import './index.styl'
 import BlogPost from "./components/blog-post";
 
-export default class Index extends Component {
-  state = {
-    isFold: [true, true, true, true, true, true]
-  }
-
-  setIsFold (index) {
-    const list = [...this.state.isFold]
-    list[index] = !list[index]
-    this.setState({
-      isFold: list
-    })
-  }
-
-  render () {
-    const isFold = this.state.isFold
-    return (
-      <body>
-        <BlogPost />
-      </body>
-    )
-  }
+export default function Index() {
+  return (
+    <body>
+      <BlogPost />
+    </body>
+  )
 }
