34493c9286f1d3602c5c4464e64444e00a5aae58
shenghanqin <shenghanqin@163.com>
2021-06-14 16:33:52 +0800

    update 更新文章获取

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,24 @@
-import { Component } from 'react'
+import Taro from "@tarojs/taro";
+import { useEffect, useState } from 'react'
 import './index.styl'
 import BlogPost from "./components/blog-post";
 
 export default function Index() {
+  const [allPosts, setAllPosts] = useState([])
+
+  useEffect(() => {
+    Taro.request({
+      url: 'https://www.xiaoxili.com/posts.json'
+    }).then(res => {
+      console.log('res', res)
+      setAllPosts(res.data)
+    })
+  }, [])
+
+  const heroPost = allPosts[0] || {}
+  const morePosts = allPosts.slice(1) || {}
+  console.log('heroPost', heroPost, morePosts)
+
   return (
     <body>
       <BlogPost />
