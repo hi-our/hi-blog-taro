45ca5ef3dfde8808217f6081632ae4a29ec0a12a
shenghanqin <shenghanqin@163.com>
2021-06-14 16:20:56 +0800

    update 更新尺寸效果

--- config/index.js
+++ config/index.js
@@ -1,11 +1,12 @@
 const config = {
   projectName: 'hi-blog-taro',
   date: '2021-6-14',
-  designWidth: 750,
+  designWidth: 375,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2
+    828: 1.81 / 2,
+    375: 2 / 1
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
